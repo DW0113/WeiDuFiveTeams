12909234d070f86da54024fddea08353c1da1da7
qyc <2638248024@qq.com>
2018-11-27 21:08:21 +0800

    2018/11/27/秦永聪/fragment滑动修改后

--- app/src/main/java/com/bw/movie/presenter/MainActivityPresenter.java
+++ app/src/main/java/com/bw/movie/presenter/MainActivityPresenter.java
@@ -20,6 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivityPresenter  extends AppDelegate implements View.OnClickListener{
+    //全局控件
     private ViewPager vp_main_viewpager;
     private List<Fragment> fragmentlist=new ArrayList<>();
     private ImageView im_move_fragment;
@@ -30,7 +31,7 @@ public class MainActivityPresenter  extends AppDelegate implements View.OnClickL
     public int getLayoutId() {
         return R.layout.activity_main;
     }
-   private Context context;
+     private Context context;
     @Override
     public void getContext(Context context) {
   this.context=context;
@@ -38,19 +39,25 @@ public class MainActivityPresenter  extends AppDelegate implements View.OnClickL
 
     @Override
     public void initData() {
+        //找控件
         vp_main_viewpager=(ViewPager)get(R.id.vp_main_viewpager);
         im_move_fragment = (ImageView) get(R.id.im_move_fragment);
         im_cinema_fragment = (ImageView) get(R.id.im_cinema_fragment);
         im_my_fragment = (ImageView) get(R.id.im_my_fragment);
+        //把fragment放到集合里面
         fragmentlist.add(new CinemaFragment());
         fragmentlist.add(new MovieFragment());
         fragmentlist.add(new MyFragment());
+        //适配器
         MyAdpater myAdpater = new MyAdpater(((MainActivity)context).getSupportFragmentManager());
         vp_main_viewpager.setAdapter(myAdpater);
+        //点击事件
         im_my_fragment.setOnClickListener(this);
         im_cinema_fragment.setOnClickListener(this);
         im_move_fragment.setOnClickListener(this);
+        //让他默认是0
         setClick(0);
+        //滑动的时候，跟着变
         vp_main_viewpager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int i, float v, int i1) {
@@ -68,6 +75,7 @@ public class MainActivityPresenter  extends AppDelegate implements View.OnClickL
             }
         });
     }
+    //点击事件
     @Override
     public void onClick(View view) {
         switch (view.getId()){
@@ -83,7 +91,7 @@ public class MainActivityPresenter  extends AppDelegate implements View.OnClickL
 
         }
     }
-
+  //改变图片
     public void setClick(int click) {
         vp_main_viewpager.setCurrentItem(click);
         switch (click){
@@ -94,7 +102,7 @@ public class MainActivityPresenter  extends AppDelegate implements View.OnClickL
                 break;
             case 1:
                 im_move_fragment.setImageResource(R.drawable.fragment_movie);
-                im_cinema_fragment.setImageResource(R.drawable.fragment_movie_selected);
+                im_cinema_fragment.setImageResource(R.drawable.fragment_cinema_selected);
                 im_my_fragment.setImageResource(R.drawable.fragment_my);
                 break;
             case 2:
@@ -107,7 +115,7 @@ public class MainActivityPresenter  extends AppDelegate implements View.OnClickL
 
     }
 
-
+//适配器
     class MyAdpater extends FragmentPagerAdapter {
 
         public MyAdpater(FragmentManager fm) {
