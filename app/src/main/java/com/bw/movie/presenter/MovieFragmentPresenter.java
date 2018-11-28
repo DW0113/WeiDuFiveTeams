01069afed33d83b70117fab631b92760ee244f1f
DW <717430517@qq.com>
2018-11-28 21:27:11 +0800

    DW,2018/11/27,影片页面轮播图

--- app/src/main/java/com/bw/movie/presenter/MovieFragmentPresenter.java
+++ app/src/main/java/com/bw/movie/presenter/MovieFragmentPresenter.java
@@ -3,8 +3,10 @@ package com.bw.movie.presenter;
 import android.content.Context;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import com.bw.movie.R;
+import com.bw.movie.adapter.MovieBannerAdapter;
 import com.bw.movie.adapter.MovieTypeAdapter;
 import com.bw.movie.model.MovieBean;
 import com.bw.movie.mvp.view.AppDelegate;
@@ -14,6 +16,8 @@ import com.google.gson.Gson;
 
 import java.util.List;
 
+import recycler.coverflow.RecyclerCoverFlow;
+
 /**
  * 作者：杜威
  * 日期：2018/11/28
@@ -22,6 +26,8 @@ public class MovieFragmentPresenter extends AppDelegate {
     private Context context;
     private RecyclerView rv_movie_movietype;
     private MovieTypeAdapter movieTypeAdapter;
+    private RecyclerCoverFlow rc_movie_banner;
+    private MovieBannerAdapter bannerAdapter;
 
     @Override
     public int getLayoutId() {
@@ -37,11 +43,15 @@ public class MovieFragmentPresenter extends AppDelegate {
     public void initData() {
         //初始化控件
         rv_movie_movietype = get(R.id.rv_movie_movietype);
+        rc_movie_banner = get(R.id.rc_movie_banner);
         //设置布局管理器
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(context);
         rv_movie_movietype.setLayoutManager(linearLayoutManager);
         movieTypeAdapter = new MovieTypeAdapter(context);
         rv_movie_movietype.setAdapter(movieTypeAdapter);
+
+        bannerAdapter = new MovieBannerAdapter(context);
+        rc_movie_banner.setAdapter(bannerAdapter);
         doHttpHotMovie();
         doHttpIsHot();
         doHttpComingSoon();
@@ -92,6 +102,7 @@ public class MovieFragmentPresenter extends AppDelegate {
                 MovieBean movieBean = gson.fromJson(data, MovieBean.class);
                 List<MovieBean.ResultBean> hotMovieList = movieBean.getResult();
                 movieTypeAdapter.sethotMovieList(hotMovieList);
+                bannerAdapter.setList(hotMovieList);
             }
 
             @Override
