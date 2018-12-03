ef6ecf56889741c6c742d8c9048bed6c0cb0c448
MLY <3202247804@qq.com>
2018-12-03 20:08:26 +0800

    2018-11-28 mly完成反馈,版本更新

--- app/src/main/java/com/bw/movie/presenter/MessageActivityPresenter.java
+++ app/src/main/java/com/bw/movie/presenter/MessageActivityPresenter.java
@@ -8,6 +8,7 @@ import android.widget.Toast;
 
 import com.bw.movie.R;
 import com.bw.movie.activity.MessageActivity;
+import com.bw.movie.model.MessageListsBean;
 import com.bw.movie.model.MessagesBean;
 import com.bw.movie.model.QueryMessageBean;
 import com.bw.movie.mvp.view.AppDelegate;
@@ -62,42 +63,63 @@ public class MessageActivityPresenter extends AppDelegate implements View.OnClic
         //查询是否有新消息
         doHttpMesssage(userld,sessionId);
 
+        //查询系统消息列表
+        doHttpMesssageLists(userld,sessionId);
 
 
-
-        Calendar calendar = Calendar.getInstance();
-        //获取日期的月
-        mMonth = String.valueOf(calendar.get(Calendar.MONTH) + 1);
-        //获取日期的天
-        mDay = String.valueOf(calendar.get(Calendar.DAY_OF_MONTH));
-        /**
-         * 如果小时是个位数
-         *
-         *则在前面价格“0”
-         * */
-        if (calendar.get(Calendar.HOUR) < 10) {
-            mHours = "0" + calendar.get(Calendar.HOUR);
-        } else {
-            mHours = String.valueOf(calendar.get(Calendar.HOUR));
-        }
-        /**
-         * 如果分钟是个位数
-         *
-         *则在前面价格“0”
-         * */
-        if (calendar.get(Calendar.MINUTE) < 10) {
-            mMinute = "0" + calendar.get(Calendar.MINUTE);
-        } else {
-            mMinute = String.valueOf(calendar.get(Calendar.MINUTE));
-        }
-        tv_activity_message_timeyear.setText(mMonth+"-"+mDay+"  "+mHours+":"+mMinute);
-        tv_activity_message_yeardaytime.setText(mMonth+"-"+mDay+"  "+mHours+":"+mMinute);
+//        Calendar calendar = Calendar.getInstance();
+//        //获取日期的月
+//        mMonth = String.valueOf(calendar.get(Calendar.MONTH) + 1);
+//        //获取日期的天
+//        mDay = String.valueOf(calendar.get(Calendar.DAY_OF_MONTH));
+//        /**
+//         * 如果小时是个位数
+//         *
+//         *则在前面价格“0”
+//         * */
+//        if (calendar.get(Calendar.HOUR) < 10) {
+//            mHours = "0" + calendar.get(Calendar.HOUR);
+//        } else {
+//            mHours = String.valueOf(calendar.get(Calendar.HOUR));
+//        }
+//        /**
+//         * 如果分钟是个位数
+//         *
+//         *则在前面价格“0”
+//         * */
+//        if (calendar.get(Calendar.MINUTE) < 10) {
+//            mMinute = "0" + calendar.get(Calendar.MINUTE);
+//        } else {
+//            mMinute = String.valueOf(calendar.get(Calendar.MINUTE));
+//        }
+//        tv_activity_message_timeyear.setText(mMonth+"-"+mDay+"  "+mHours+":"+mMinute);
+//        tv_activity_message_yeardaytime.setText(mMonth+"-"+mDay+"  "+mHours+":"+mMinute);
 
 
 
 
     }
+    //查询系统消息列表
+    private void doHttpMesssageLists(String userld, String sessionId) {
+        Map<String,String> map=new HashMap<>();
+        map.put("userId",userld);
+        map.put("sessionId",sessionId);
+        map.put("page","1");
+        map.put("count","10");
+        new Utility().get("movieApi/tool/v1/verify/findAllSysMsgList",map).result(new HttpListener() {
+            @Override
+            public void success(String data) {
+                MessageListsBean messageListsBean = new Gson().fromJson(data, MessageListsBean.class);
 
+            }
+
+            @Override
+            public void fail(String error) {
+
+            }
+        });
+    }
+    //查询是否有新消息
     private void doHttpMesssage(String userld,String sessionId) {
         Map<String,String> map=new HashMap<>();
         map.put("userId",userld);
@@ -119,8 +141,7 @@ public class MessageActivityPresenter extends AppDelegate implements View.OnClic
 
     private void init() {
         tv_activity_message_unread=(TextView)get(R.id.tv_activity_message_unread);
-        tv_activity_message_yeardaytime=(TextView)get(R.id.tv_activity_message_yeardaytime);
-        tv_activity_message_timeyear=(TextView)get(R.id.tv_activity_message_timeyear);
+
     }
 
     @Override
